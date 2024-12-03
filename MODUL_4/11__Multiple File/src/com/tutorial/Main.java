34a5923e48eb7d75669e8060c2e5e7dc5fae0895
Meyesss <146051966+Meyesss@users.noreply.github.com>
2024-12-03 12:46:38 +0700

    Update Main.java

--- MODUL_4/11__Multiple File/src/com/tutorial/Main.java
+++ MODUL_4/11__Multiple File/src/com/tutorial/Main.java
@@ -7,9 +7,9 @@ import static com.terminal.Console.log;
 public class Main {
     public static void main(String[] args) {
         // membuat objek
-        Player player1 = new Player("Mustiati");
-        Player player2 = new Player("Aulia Zaskia");
-        Player player3 = new Player("Erna Wati");
+        Player player1 = new Player("Ika");
+        Player player2 = new Player("Damayanti");
+        Player player3 = new Player("Yanti");
         player1.cetak();
         player2.cetak();
         player3.cetak();
